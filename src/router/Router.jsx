f0d367d8441635ab4bc868a599bb7b644e1cb0c4
parhamfarnay <prhmfrny@gmail.com>
2024-09-18 18:28:24 +0330

    feat/interceptor created for api

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,11 +1,15 @@
 import { Route, Routes } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
 import HomePage from "../pages/HomePage";
 import DashboardPage from "../pages/DashboardPage";
 import AuthPage from "../pages/AuthPage";
 import AdminPage from "../pages/AdminPage";
 import PageNotFound from "../pages/404";
+import { getProfile } from "services/user";
 
 function Router() {
+  const { data, isLoading } = useQuery(["profile"], getProfile);
+  if (isLoading) return <h1>Loading...</h1>;
   return (
     <Routes>
       <Route index element={<HomePage />} />
