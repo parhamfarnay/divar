8914085b61ee900702a074154ab1fc11f7247357
parhamfarnay <prhmfrny@gmail.com>
2024-09-21 03:47:54 +0330

    feat/postlist added to project

--- src/components/templates/CategoryList.jsx
+++ src/components/templates/CategoryList.jsx
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import { getCategory } from "services/admin";
 import styles from "./CategoryList.module.css";
+import { LoaderIcon } from "react-hot-toast";
 
 function CategoryList() {
   const { data, isloading } = useQuery(["get-categories"], getCategory);
@@ -8,7 +9,7 @@ function CategoryList() {
   return (
     <div className={styles.list}>
       {isloading ? (
-        <Loader />
+        <LoaderIcon />
       ) : (
         data.data.map((i) => (
           <div key={i._id}>
